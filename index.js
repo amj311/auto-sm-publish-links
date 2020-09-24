4137156bff51b1ede34d8e024008c8f0f1c49634
amj311 <amjudd315@gmail.com>
2020-09-24 10:06:18 -0600

    added vue and bootstrap. v-model for publication on textarea, p, and character counter

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+    el: "#app",
+
+    data: {
+        publication: "Write your post and use the links below to send it anywhere!\n\nMade with 💗 by amj311",
+    },
+
+    mounted() {
+    }
+})
