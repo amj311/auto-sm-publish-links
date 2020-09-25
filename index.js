0f7d1dd985a16b888c2ad946ec73dcca9cc54af8
amj311 <amjudd315@gmail.com>
2020-09-24 18:27:52 -0600

    removed fb. added copy button and encoded display. network goes grey if too many characters

--- index.js
+++ index.js
@@ -5,23 +5,34 @@ const app = new Vue({
         networks: [
             {
                 displayName: "Twitter",
-                maxChar: 80,
+                maxChar: 280,
                 publishURL: "https://twitter.com/intent/tweet?text=",
                 brandColor: "#1da1f2",
                 brandFaCode: "fa fa-twitter",
             },
-            {
-                displayName: "Facebook",
-                maxChar: Infinity,
-                publishURL: "https://facebook.com/intent...",
-                brandColor: "#3b5998",
-                brandFaCode: "fa fa-facebook",
-            },
+            // {
+            //     displayName: "Facebook",
+            //     maxChar: Infinity,
+            //     publishURL: "https://facebook.com/intent...",
+            //     brandColor: "#3b5998",
+            //     brandFaCode: "fa fa-facebook",
+            // },
         ],
-        publication: "Write your post and use the links below to send it anywhere!\n\nMade with 💗 by amj311",
+        publication: "Write your post and use the links below to send it anywhere!\n\nMade with 🥑 by amj311",
     },
 
-    mounted() {
+    methods: {
+        copyNetworkLink(name) {
+            linktext = this.$refs['link_'+name][0];
+            linktext.select();
+            linktext.setSelectionRange(0, 99999); /*For mobile devices*/
+
+            /* Copy the text inside the text field */
+            document.execCommand("copy");
+          
+            /* Alert the copied text */
+            alert("Copied the text: " + linktext.value);
+        }
     },
 
     computed: {
